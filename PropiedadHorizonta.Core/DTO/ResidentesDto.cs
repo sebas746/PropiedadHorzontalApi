ae1a8a0904df4cb71fb846c10240304ca979116a
jortegona <jortegona@psl.com.co>
2020-04-19 17:55:18 -0500

    Residentes dto added.

---
+++ PropiedadHorizonta.Core/DTO/ResidentesDto.cs
@@ -0,0 +1,16 @@
+﻿namespace PropiedadHorizontal.Core.DTO
+{
+    public class ResidentesDto
+    {
+        public string IdDocumentoResidente { get; set; }
+        public int IdTipoDocumentoResidente { get; set; }
+        public string NombreTipoDocumento { get; set; }
+        public string DescripcionTipoDocumento { get; set; }
+        public string NombresResidente { get; set; }
+        public string ApellidosResidente { get; set; }
+        public string CelularResidente { get; set; }
+        public string EmailResidente { get; set; }
+        public string GeneroResidente { get; set; }
+        public TipoDocumentosDto TipoDocumento { get; set; }
+    }
+}
